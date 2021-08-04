b535e3bcaca5579cea0597fe9d8f15497936991a
Aventail141 <87988732+Aventail141@users.noreply.github.com>
2021-08-04 10:18:15 +0900

    Add files via upload

---
+++ ToDoListSource/app.js
@@ -0,0 +1,61 @@
+// 閉じるボタンを作成し、各リストアイテムに付加する
+var mylist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < mylist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  mylist[i].appendChild(span);
+}
+
+// クローズボタンをクリックすると、現在のリストアイテムが非表示になる
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// リストアイテムをクリックしたときにチェックマークを付ける
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// 追加ボタンをクリックすると、新しいリストアイテムが作成される
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("なにか入力してください");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+/*
+-------------------
+  END OF PROJECT
+-------------------
+*/
